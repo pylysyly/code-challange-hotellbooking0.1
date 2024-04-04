01141eb6ea042cb1cb7f0ed625a8d21af0be829e
bjornimo <bjornimo@arcada.fi>
2024-04-04 08:42:12 +0000

    initial

---
+++ hotel.api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS 
+
+PORT=5000
+
+app = Flask(__name__)
+CORS(app) # Tillåt cross-origin requests
+
+@app.route("/")
+def hello():
+    return "<h1>Hello, World!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
+        '/etc/letsencrypt/fullchain.pem', 
+        '/etc/letsencrypt/privkey.pem'
+    ))
